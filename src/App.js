0cefd4acab7b2f0902cd7d34fc988988b8891d7c
abhinaygoud2004 <abhinaygoud2004@gmail.com>
2024-03-10 12:52:10 +0530

    made changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import io from 'socket.io-client'
 import {useEffect,useState} from "react"
-const socket=io.connect("http://localhost:3001");
-// const socket=io.connect("https://abhi-chatapp.netlify.app");
+// const socket=io.connect("http://localhost:3001");
+const socket=io.connect("https://abhi-chatapp.netlify.app");
 
 
 function App() {
